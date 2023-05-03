b5f414159893a37eceb9bfcf98a8ae4b43ec2c3c
TangHoang <businesshoangtang@gmail.com>
2023-05-03 16:14:00 +0200

    add pinia support

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
 
+const pinia = createPinia();
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
