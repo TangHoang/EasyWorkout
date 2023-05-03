ec6a080dca88462a51f3fba8f0905617147d1793
TangHoang <businesshoangtang@gmail.com>
2023-05-03 17:05:06 +0200

    change path for logView

--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-history: createWebHistory(import.meta.env.BASE_URL),
-routes: [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/exercise-log',
-        name: 'exercise-log',
-        component: () => import('../views/LogView.vue')
-    },
-    {
-        path: '/calendar',
-        name: 'calendar',
-        component: () => import('../views/CalendarView.vue')
-    },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/logs',
+            name: 'logs',
+            component: () => import('../views/LogView.vue')
+        },
+        {
+            path: '/calendar',
+            name: 'calendar',
+            component: () => import('../views/CalendarView.vue')
+        },
     ]
 })
 
