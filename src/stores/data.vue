eb861383b648c66d44902fe90d464e23906e20f4
TangHoang <businesshoangtang@gmail.com>
2023-05-03 15:55:13 +0200

    add pinia support

---
+++ src/stores/data.vue
@@ -0,0 +1,31 @@
+<script>
+    import { ref, computed } from 'vue'
+    import { defineStore } from 'pinia'
+
+    export const useDataStore = defineStore('data', {
+        state: () => {
+            return {
+                data: {"Bench Press": 
+                        {sets: {
+                            "3.05.2023": [{set: 1, weight: 100, reps: 10}, {set: 2, weight: 100, reps: 8}, {set: 1, weight: 100, reps: 6}],
+
+                            }
+                        },
+                       "Squat": 
+                        {sets: {
+                            "03.05.2023": [{set: 1, weight: 120, reps: 10}, {set: 2, weight: 120, reps: 8}, {set: 1, weight: 120, reps: 6}]
+                            }
+                        },
+                },
+                date: "",
+            } 
+        },
+        actions: {
+            getDate(){
+                let temp = new Date()
+                this.date = temp.toLocaleDateString("de-DE", {dateStyle: "medium"});
+            }
+        }
+    })
+</script>
+
