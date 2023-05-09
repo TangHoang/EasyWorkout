58109edacece2d94fc56345a3f7404dcd635022d
TangHoang <businesshoangtang@gmail.com>
2023-05-09 21:51:19 +0200

    fix: fix wrong sample data

--- src/stores/data.vue
+++ src/stores/data.vue
@@ -7,13 +7,13 @@
             return {
                 data: {"Bench Press": 
                         {sets: {
-                            "3.05.2023": [{set: 1, weight: 100, reps: 10}, {set: 2, weight: 100, reps: 8}, {set: 1, weight: 100, reps: 6}],
+                            "3.05.2023": [{set: 1, weight: 100, reps: 10}, {set: 2, weight: 100, reps: 8}, {set: 3, weight: 100, reps: 6}],
 
                             }
                         },
                        "Squat": 
                         {sets: {
-                            "03.05.2023": [{set: 1, weight: 120, reps: 10}, {set: 2, weight: 120, reps: 8}, {set: 1, weight: 120, reps: 6}]
+                            "03.05.2023": [{set: 1, weight: 120, reps: 10}, {set: 2, weight: 120, reps: 8}, {set: 2, weight: 120, reps: 6}]
                             }
                         },
                 },
