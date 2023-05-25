790b16a353de576c8513679825a67910d2d668ba
TangHoang <businesshoangtang@gmail.com>
2023-05-25 19:45:46 +0200

    change url for access control allow origin

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 // Enable CORS middleware
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://tanghoang.github.io/EasyWorkout/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
