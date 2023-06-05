eaf735848be1faa187499d01bcf5fbd523dca995
TangHoang <businesshoangtang@gmail.com>
2023-06-05 11:17:08 +0200

    fix middleware auth routes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/EasyWorkout/index.html"));
 });
 app.use("/", express.static(__dirname));
-app.use("https://easyworkout-production.up.railway.app/auth", authRoutes);
+app.use("/auth", authRoutes);
 
 // schemas
 const dataSchema = new mongoose.Schema({
