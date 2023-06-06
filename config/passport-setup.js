08c7db08749b123948cd449890f781bfe41476cb
TangHoang <businesshoangtang@gmail.com>
2023-06-06 17:08:09 +0200

    add retrieve and save user function

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/user-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id); // user.id is the id in the database
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    })
+});
 
 passport.use( 
     new GoogleStrategy({
@@ -8,7 +19,20 @@ passport.use(
     callbackURL: '/auth/google/redirect',
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    }, () => {
-    // passport callback function
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                console.log("user is: " + currentUser);
+                done(null, currentUser);
+            }else {     
+                new User({
+                    name: profile.displayName,
+                    googleId: profile.id,
+                }).save().then((newUser) => {  
+                    console.log("new user created: " + newUser);
+                    done(null, newUser);
+                });
+            }
+        });
     }
 ));
