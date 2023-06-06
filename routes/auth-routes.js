dc5912336af9ec8f682b160fee0908116dbcd8ae
TangHoang <businesshoangtang@gmail.com>
2023-06-06 17:08:57 +0200

    add google redirect route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -16,5 +16,9 @@ router.get('/logout', (req, res) => {
     res.send("logging out");
 });
 
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => { // passport.authenticate again, to decrypt "code/token" from google and grab info
+    res.send("you reached the redirect URI");
+});
+
 module.exports = router;
 
