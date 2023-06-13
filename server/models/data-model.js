8f8bd795aa37cbc5d525a90b5f779d8e8b78929b
TangHoang <businesshoangtang@gmail.com>
2023-06-13 11:50:26 +0200

    remove _id and __v from objects when getting data

--- server/models/data-model.js
+++ server/models/data-model.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
+    _id: false,
     date: Object,
-}, {strict: false});
+});
 
 const historySchema = new mongoose.Schema({
+    _id: false,
     date: Object,
-}, {strict: false});
+});
 
 const trainingdataSchema = new mongoose.Schema({
     data: dataSchema,
@@ -15,5 +17,4 @@ const trainingdataSchema = new mongoose.Schema({
 });
 
 const trainingdata = mongoose.model('trainingdata', trainingdataSchema);
-
 module.exports = trainingdata;
