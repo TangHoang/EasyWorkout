ba84dd771858c0a06a41d96f4952f5114a88fc2e
TangHoang <businesshoangtang@gmail.com>
2023-06-13 12:45:10 +0200

    implement different dataset for each user

--- models/data-model.js
+++ models/data-model.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema({
     _id: false,
     date: Object,
-});
+}, {strict: false});
 
 const historySchema = new mongoose.Schema({
     _id: false,
     date: Object,
-});
+}, {strict: false});
 
 const trainingdataSchema = new mongoose.Schema({
     data: dataSchema,
