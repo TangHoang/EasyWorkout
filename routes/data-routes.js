b1d675a5148846b53b8919ccfaf7030512f4e362
TangHoang <businesshoangtang@gmail.com>
2023-06-15 12:31:44 +0200

    add demo account support

--- routes/data-routes.js
+++ routes/data-routes.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const trainingdata = require('../models/data-model');
 
 router.get("/get", (req, res, next) => {
+    if(req.user == undefined) {
+        req.user = {_id: "648adf185d512e841aef650a",
+                    name: "Demo",
+                    };
+    }
     trainingdata.findOne({_id: req.user._id}, {_id: 0, __v: 0})
         .then(trainingdata => {
             if(trainingdata) {
@@ -19,13 +24,18 @@ router.get("/get", (req, res, next) => {
 })
 
 router.post("/post", (req, res, next) => {
-    console.log(req.user._id);
+    console.log(req.user);
+    if(req.user == undefined) {
+        req.user = {_id: "648adf185d512e841aef650a",
+                    name: "Demo",
+                    };
+    }
     const newTrainingdata = new trainingdata(req.body);
     newTrainingdata._id = req.user._id;
     const existingTrainingdata = trainingdata.findOne({_id: req.user._id}, {_id: 1})
         .then(existingTrainingdata => {
             if(existingTrainingdata) {
-                let objectId = mongoose.Types.ObjectId(req.user._id);
+                let objectId = new mongoose.Types.ObjectId(req.user._id);
                 trainingdata.findByIdAndDelete(objectId)
                     .then(() => {
                         console.log("Deleted existing data");
